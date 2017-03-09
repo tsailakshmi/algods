9b2a08c9afeb2fc6335af059b56fda100f03601e
Phani Kumar Tumuluri <ptumuluri@ptumuluri.hyd.corp.google.com>
2017-03-09 15:20:29 +0530

    reverse a linked list

--- ds/linkedlist/LinkedList.java
+++ ds/linkedlist/LinkedList.java
@@ -23,6 +23,13 @@ class LinkedList {
       }
       current.next = new Node(data);
   }
+
+  
+  public void prepend(int data) {
+      Node current = head;
+      head = new Node(data);
+      head.next = current;
+  }
   
   public void printAll() {
       Node current = head;
@@ -35,6 +42,7 @@ class LinkedList {
   
   public void delete(int data) {
     if (head.data == data) {
+      System.out.println(data + " deleted");
       head = head.next;
       return;
     }
@@ -48,19 +56,53 @@ class LinkedList {
     }
   }
   
+  public void reverse() {
+    Node previous = null;
+    Node current = head;
+    while (current != null) {
+      head = current.next;
+      current.next = previous;
+      previous = current;
+      current = head;
+    }
+    head = previous;
+  }
+
+  public void recursiveReverse(Node currentNode) {
+    if (currentNode.next == null) {
+      head = currentNode;
+      return;
+    }
+    recursiveReverse(currentNode.next);
+    currentNode.next.next = currentNode;
+    currentNode.next = null;
+  }
+  
   public static void main(String args[]) {
 
     LinkedList linkedList = new LinkedList();
     
-    linkedList.append(1);
-    linkedList.append(2);
-    linkedList.append(4);
-    linkedList.append(3);
+    int[] elements = {1, 2, 3, 4};
+    for (int i = 0; i < elements.length; i++) {
+      linkedList.append(elements[i]); 
+    }    
     
+    System.out.println("Elements in linked list:");
     linkedList.printAll();
     
-    linkedList.delete(1);
+    System.out.println("Reversing elements:");
+    linkedList.reverse();
+    System.out.println("Elements in linked list after reversing:");
+    linkedList.printAll();
     
+    System.out.println("Reversing elements recursively:");
+    linkedList.recursiveReverse(linkedList.head);
+    System.out.println("Elements in linked list after reversing:");
+    linkedList.printAll();
+    
+    System.out.println("Deleting 1 from linked list:");
+    linkedList.delete(1);
+    System.out.println("Elements in linked list after deleting 1:");
     linkedList.printAll();
   }
 }
